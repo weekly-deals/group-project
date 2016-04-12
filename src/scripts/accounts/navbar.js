ab3e92ed7429c4fefa4e2f842676d196c45edd1a
Brian Kemper <bkemper@gmail.com>
2016-04-12 15:35:53 -0600

    svg stuff

--- src/scripts/accounts/navbar.js
+++ src/scripts/accounts/navbar.js
@@ -1,7 +1,11 @@
 angular.module('app')
-    .controller('NavbarCtrl', function ($scope, $auth, NgMap, geoService) {
+    .controller('NavbarCtrl', function ($scope, $auth, NgMap, geoService, $location, svgService) {
 
         var vm = this;
+
+        vm.hideDealBar = function () {
+            return !(/maps|login|signup/.test($location.url()))
+        };
 
         vm.isAuthenticated = function () {
             return $auth.isAuthenticated();
@@ -9,11 +13,68 @@ angular.module('app')
 
         vm.types = "['establishment']";
 
-        geoService.getCurrentPosition().then(function(latlng){
-            geoService.reverseGeoCode(latlng).then(function(city){
+        geoService.getCurrentPosition().then(function (latlng) {
+            geoService.reverseGeoCode(latlng).then(function (city) {
                 vm.city = city;
             })
         });
+
+        function svgs() {
+            svgService.getSvg().then(function (res) {
+                vm.svgs = res;
+            })
+        }
+
+        svgs();
+
+        vm.days = [
+            {
+                display: 'Sunday',
+                val: function () {
+                    return {day: 0};
+                }
+            },
+            {
+                display: 'Monday',
+                val: function () {
+                    return {day: 1};
+                }
+            },
+            {
+                display: 'Tuesday',
+                val: function () {
+                    return {day: 2};
+                }
+            },
+            {
+                display: 'Wednesday',
+                val: function () {
+                    return {day: 3};
+                }
+            },
+            {
+                display: 'Thursday',
+                val: function () {
+                    return {day: 4};
+                }
+            },
+            {
+                display: 'Friday',
+                val: function () {
+                    return {day: 5};
+                }
+            },
+            {
+                display: 'Saturday',
+                val: function () {
+                    return {day: 6};
+                }
+            }
+        ];
+
+        var date = new Date();
+        vm.dayNum = date.getDay();
+        vm.selectedDay = vm.days[vm.dayNum];
 
         vm.placeChanged = function () {
             vm.place = this.getPlace();
@@ -23,7 +84,6 @@ angular.module('app')
                 map: vm.map,
                 title: vm.place.name
             });
-
         };
 
         vm.addBusiness = function () {
