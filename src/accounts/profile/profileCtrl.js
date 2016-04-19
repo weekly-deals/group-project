f76cf401ea55247b9bfe5522c18d9bb0951f1aee
Brian Kemper <bkemper@gmail.com>
2016-04-19 12:08:35 -0600

    redid modals, hopefully fixed login, and cleaned up everything

--- src/accounts/profile/profileCtrl.js
+++ src/accounts/profile/profileCtrl.js
@@ -1,71 +1,69 @@
 angular.module('app')
-    .controller('profileCtrl', function ($scope, $auth, Account, geoService) {
+    .directive('profileDir', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/profile.html',
+            scope: {
+                show: '='
+            },
+            link: function (scope) {
+                scope.closeProfile = function () {
+                    scope.show = false;
+                    var body = document.getElementById('body');
+                    body.style.overflow = 'auto';
+                }
+            },
+            controller: function ($scope, $auth, Account) {
 
+                $scope.getProfile = function () {
+                    Account.getProfile()
+                        .then(function (response) {
+                            $scope.user = response.data;
+                        })
+                        .catch(function (response) {
+                            // toastr.error(response.data.message, response.status);
+                        });
+                };
 
+                $scope.updateProfile = function () {
+                    $scope.user.picture = Account.userPic;
+                    console.log($scope.user.picture);
+                    Account.updateProfile($scope.user)
 
-        $scope.getProfile = function () {
-            Account.getProfile()
-                .then(function (response) {
-                    $scope.user = response.data;
-                })
-                .catch(function (response) {
-                    // toastr.error(response.data.message, response.status);
-                });
-        };
-        $scope.updateProfile = function () {
-            $scope.user.picture = Account.userPic
-            console.log($scope.user.picture)
-            Account.updateProfile($scope.user)
+                        .then(function () {
+                            // toastr.success('Profile has been updated');
+                            $scope.getProfile();
+                        }).then(function(){
+                        $scope.closeProfile()
+                    })
+                        .catch(function (response) {
+                            // toastr.error(response.data.message, response.status);
+                        });
+                };
 
-                .then(function () {
-                    // toastr.success('Profile has been updated');
-                    $scope.getProfile();
+                $scope.link = function (provider) {
+                    $auth.link(provider)
+                        .then(function () {
+                            // toastr.success('You have successfully linked a ' + provider + ' account');
+                            $scope.getProfile();
+                        })
+                        .catch(function (response) {
+                            // toastr.error(response.data.message, response.status);
+                        });
+                };
 
-                })
-                .catch(function (response) {
-                    // toastr.error(response.data.message, response.status);
-                });
-        };
-        $scope.link = function (provider) {
-            $auth.link(provider)
-                .then(function () {
-                    // toastr.success('You have successfully linked a ' + provider + ' account');
-                    $scope.getProfile();
-                })
-                .catch(function (response) {
-                    // toastr.error(response.data.message, response.status);
-                });
-        };
-        $scope.unlink = function (provider) {
-            $auth.unlink(provider)
-                .then(function () {
-                    // toastr.info('You have unlinked a ' + provider + ' account');
-                    $scope.getProfile();
-                })
-                .catch(function (response) {
-                    // toastr.error(response.data ? response.data.message : 'Could not unlink ' + provider + ' account', response.status);
-                });
-        };
+                $scope.unlink = function (provider) {
+                    $auth.unlink(provider)
+                        .then(function () {
+                            // toastr.info('You have unlinked a ' + provider + ' account');
+                            $scope.getProfile();
+                        })
+                        .catch(function (response) {
+                            // toastr.error(response.data ? response.data.message : 'Could not unlink ' + provider + ' account', response.status);
+                        });
+                };
 
-        $scope.getProfile();
-
-        //Modal controls//
-        $scope.showMe = function() {
-          var promodal = document.getElementById('promodal');
-          var body = document.getElementById('body');
-          var curtain = document.getElementById('promodal-curtain');
-          curtain.style.display = 'block';
-          body.style.overflow = 'hidden';
-          promodal.style.display = 'block';
-        };
-
-        $scope.closeClick = function() {
-          var promodal = document.getElementById('promodal');
-          promodal.style.display = 'none';
-          var body = document.getElementById('body');
-          body.style.overflow = 'auto';
-          var curtain = document.getElementById('promodal-curtain');
-          curtain.style.display = 'none';
-        };
-
+                $scope.getProfile();
+            }
+        }
     });
---
+++ src/dealDetailDir/dealDetailDir.js
@@ -0,0 +1,21 @@
+angular.module('app')
+    .directive('dealDetailDir', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/dealDetail.html',
+             scope: {
+                 show: '=',
+                 deal: '='
+             },
+            link: function (scope) {
+                scope.closeDetails = function(){
+                    scope.show = false;
+                    var body = document.getElementById('body');
+                    body.style.overflow = 'auto';
+                }
+            },
+            controller: function($scope){
+                $scope.test = 'test';
+            }
+        }
+    });
