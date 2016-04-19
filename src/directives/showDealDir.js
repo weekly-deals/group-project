41cabecc221d07fecaa817bfc92a4000d77ae219
Joseph Lago <josephlago90@gmail.com>
2016-04-19 11:12:21 -0600

    pagination

---
+++ src/directives/showDealDir.js
@@ -0,0 +1,13 @@
+// angular.module('app')
+// .directive('showDeal', function(){
+//   return {
+//     restrict: 'E',
+//     templateUrl: '/showDeal.html',
+//     controller: 'ModalCtrl',
+//     controllerAs: 'vm',
+// 		scope: {
+// 			deal: "="
+// 		}
+// 	// 	replace: true
+//   }
+// });
