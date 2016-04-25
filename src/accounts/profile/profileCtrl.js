e5dd7e4a2011bda7d499ef191b46073e1ee170bc
Brian Kemper <bkemper@gmail.com>
2016-04-25 11:11:36 -0600

    fixing gradient

--- src/accounts/profile/profileCtrl.js
+++ src/accounts/profile/profileCtrl.js
@@ -32,7 +32,7 @@ angular.module('app')
                   }
                     var backgroundsToChange = document.getElementsByClassName('change-color');
                     var gradientChange = document.getElementsByClassName('gradient-change');
-                    gradientChange[0].style.background = 'linear-gradient(' + color + ', transparent)';
+                    gradientChange[0].style.background = 'linear-gradient(' + color + ',' + color + ',' + color +', transparent)';
                     var changeTextColor = document.getElementsByClassName('change-text-color');
                     var borderChange = document.getElementsByClassName('change-border');
                     Array.prototype.forEach.call(borderChange, function(elem) {
